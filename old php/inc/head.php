689cc5cc0899c99ba09506679492f5362990fd86
billcopes <bill@mothdesign.net>
2016-11-03 16:17:40 -0400

    PHP to HTML
    Move site from php to html.  fix stuff

--- old php/inc/head.php
+++ old php/inc/head.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="title" content="MITAA - Redesign UI Archive">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
 	<link rel="shortcut icon" href="http://www.mothdesign.net/images/favicon.ico">
 	<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 	<link rel="stylesheet" href="lightbox.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://use.typekit.net/xln6aya.js"></script>
 	<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
 	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 	<script>
